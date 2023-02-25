3f1000486de181a5742b35e3ba2eebf0c01275de
mxnkng <alsrud_9810@naver.com>
2023-02-25 18:40:40 +0900

    init

---
+++ PreOnBoarding/PreOnBoarding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PreOnBoarding
+//
+//  Created by 이민경 on 2023/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
